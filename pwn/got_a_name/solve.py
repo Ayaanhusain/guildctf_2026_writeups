e19b038d2ed3f7ae0296ba559189796a045ce434
Ayaanhusain <50568156+Ayaanhusain@users.noreply.github.com>
2026-09-19 12:39:46 +0530

    Update solve.py

--- pwn/got_a_name/solve.py
+++ pwn/got_a_name/solve.py
@@ -1,11 +1,15 @@
 from pwn import *
+import sys
 
 context.binary = elf = ELF('./chal')
 context.log_level = 'info'
 
+HOST = sys.argv[1] if len(sys.argv) > 1 else None
+PORT = int(sys.argv[2]) if len(sys.argv) > 2 else None
+
 def start():
-    if args.REMOTE:
-        return remote('10.21.232.223', 45787)
+    if HOST and PORT:
+        return remote(HOST, PORT)
     return process('./chal')
 
 p = start()
